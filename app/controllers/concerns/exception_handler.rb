253ef8640c441ae7825e3de3436d3056bfc10b30
Tanner Dale <tanner@tannerdale.com>
2021-10-22 22:15:48 -0500

    Refactor error message

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,11 +3,10 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: { errors: { details: e.message } }
-    end
-
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: { message: 'your query could not be completed', errors: { details: e.message } }
+      render json: {
+        message: 'your query could not be completed',
+        errors: { details: e.message }
+      }, status: :not_found
     end
   end
 end
