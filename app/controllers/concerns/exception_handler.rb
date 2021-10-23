3606af31b9d8563e0bca09c8852100fb06c5c34b
Tanner Dale <tanner@tannerdale.com>
2021-10-23 00:05:11 -0500

    Add parameter missing, record invalid exception handling

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,11 +2,25 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      render json: {
+        message: 'Invalide Parameters',
+        errors: { details: e.message }
+      }, status: 400
+    end
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: {
         message: 'your query could not be completed',
         errors: { details: e.message }
       }, status: :not_found
+    end
+
+    rescue_from ActionController::ParameterMissing do |e|
+      render json: {
+        message: 'Missing Parameter',
+        errors: { details: e.message }
+      }, status: 400
     end
   end
 end
