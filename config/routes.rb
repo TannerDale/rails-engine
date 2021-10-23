d8ffd7fcc9db83a9cb096aa1f13febaa4ce28dda
Tanner Dale <tanner@tannerdale.com>
2021-10-23 00:06:06 -0500

    Add items update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :items, only: :index
       end
 
-      resources :items, only: :create
+      resources :items, only: %i[create update]
     end
   end
 end
