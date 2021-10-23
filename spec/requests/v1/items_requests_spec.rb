5f49090b7cc7c74cae56fca6077c56ff2f74f81d
Tanner Dale <tanner@tannerdale.com>
2021-10-23 00:06:22 -0500

    Add merchant item index tests

--- spec/requests/v1/items_requests_spec.rb
+++ spec/requests/v1/items_requests_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Api::V1::ItemsController do
   let(:merchant) { create :merchant }
-  let!(:params) do
+  let(:params) do
     {
       item: {
         name: 'value1',
@@ -15,6 +15,29 @@ describe Api::V1::ItemsController do
   let(:json) { JSON.parse(response.body, symbolize_names: true) }
   let(:data) { json[:data] }
   let(:attributes) { data[:attributes] }
+
+  describe 'GET /v1/:merchant_id/items' do
+    let!(:merchant2) { create :merchant }
+    let!(:items) { create_list :item, 10, { merchant_id: merchant.id } }
+    let!(:other_items) { create_list :item, 4, { merchant_id: merchant2.id } }
+
+    context 'with valid merchant id' do
+      before { get api_v1_merchant_items_path(merchant.id) }
+
+      it 'returns the merchants items' do
+        expect(data.size).to eq(10)
+        expect(data.first[:id].to_i).to eq(items.first.id)
+      end
+    end
+
+    context 'with invalid merchant id' do
+      before { get api_v1_merchant_items_path(100_000) }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 
   describe 'POST /v1/items' do
     context 'with valid params' do
@@ -38,9 +61,46 @@ describe Api::V1::ItemsController do
     context 'with invalid params' do
       before { post api_v1_items_path, params: { item: { hell: 'world' } } }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
+      it 'returns status code 400' do
+        expect(response).to have_http_status(400)
         expect(Item.count).to eq(0)
+      end
+    end
+  end
+
+  describe 'PATCH /v1/:merchant_id/items' do
+    let!(:merchant) { create :merchant }
+    let!(:item) { create :item, { merchant_id: merchant.id } }
+    let(:params) do
+      {
+        item: {
+          name: 'Hello'
+        }
+      }
+    end
+
+    context 'with valid params' do
+      before { patch api_v1_item_path(item), params: params }
+
+      it 'updates the item' do
+        item.reload
+
+        expect(Item.last.name).to eq('Hello')
+        expect(data).to have_key(:id)
+        expect(attributes).to have_key(:name)
+        expect(attributes[:unit_price]).to eq(item.unit_price)
+      end
+    end
+
+    context 'with invalid params' do
+      before { patch api_v1_item_path(item), params: {  chicken: 'hello' } }
+
+      it 'does not update the item' do
+        expect(item).to eq(item.reload)
+      end
+
+      it 'returns status code 400' do
+        expect(response).to have_http_status(400)
       end
     end
   end
