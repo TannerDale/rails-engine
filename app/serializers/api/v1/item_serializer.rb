6f94f04647eb8f1973130845871e12c7cae4e178
Tanner Dale <tanner@tannerdale.com>
2021-10-23 22:17:33 -0500

    Delete unit price attribute block

--- app/serializers/api/v1/item_serializer.rb
+++ app/serializers/api/v1/item_serializer.rb
@@ -1,9 +1,5 @@
 class Api::V1::ItemSerializer
   include JSONAPI::Serializer
 
-  attributes :name, :description, :merchant_id
-
-  attribute :unit_price do |i|
-    i.unit_price.fdiv(100)
-  end
+  attributes :name, :description, :merchant_id, :unit_price
 end
