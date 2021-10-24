4ed60ef168df9bf350d54086d7a5fbc0f006d5d7
Tanner Dale <tanner@tannerdale.com>
2021-10-24 18:40:42 -0500

    Add searching tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,4 +20,34 @@ describe Item do
       expect(Invoice.count).to eq(0)
     end
   end
+
+  describe 'finding by name and price' do
+    let(:merchant) { create :merchant }
+    let!(:item1) { create :item, { name: 'z hello kitty', unit_price: 100, description: 'a' } }
+    let!(:item2) { create :item, { name: 'a goodbye kitty', unit_price: 200, description: 'a' } }
+    let!(:item3) { create :item, { name: 'chicken nugget', unit_price: 300, description: 'a' } }
+    let!(:item4) { create :item, { name: 'a', description: 'endorphine rush', unit_price: 400 } }
+    let!(:item5) { create :item, { name: 'c', description: 'rushed helipad', unit_price: 500 } }
+    let!(:item6) { create :item, { name: 'b', description: 'hello world', unit_price: 600 } }
+
+    it 'can find when data in name' do
+      result = Item.find_by_name('kitty')
+      expect(result).to eq([item2, item1])
+    end
+
+    it 'can find when above min price' do
+      result = Item.above_price(400)
+      expect(result).to eq([item4, item6, item5])
+    end
+
+    it 'can find when below max price' do
+      result = Item.below_price(399)
+      expect(result).to eq([item2, item3, item1])
+    end
+
+    it 'can find when within a range' do
+      result = Item.find_in_range(200, 475)
+      expect(result).to eq([item4, item2, item3])
+    end
+  end
 end
