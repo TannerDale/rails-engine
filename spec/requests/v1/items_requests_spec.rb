b6674e8ecf93788030d501fe009aff3384f5ccf1
Tanner Dale <tanner@tannerdale.com>
2021-10-23 22:19:58 -0500

    Add getting all items tests

--- spec/requests/v1/items_requests_spec.rb
+++ spec/requests/v1/items_requests_spec.rb
@@ -16,22 +16,92 @@ describe Api::V1::ItemsController do
   let(:data) { json[:data] }
   let(:attributes) { data[:attributes] }
 
-  describe 'GET /v1/:merchant_id/items' do
-    let!(:merchant2) { create :merchant }
-    let!(:items) { create_list :item, 10, { merchant_id: merchant.id } }
-    let!(:other_items) { create_list :item, 4, { merchant_id: merchant2.id } }
+  describe 'GET /v1/items' do
+    let!(:items) { create_list :item, 30, { merchant_id: merchant.id } }
 
-    context 'with valid merchant id' do
-      before { get api_v1_merchant_items_path(merchant.id) }
+    describe 'for all items' do
+      context 'without query params' do
+        before { get api_v1_items_path }
 
-      it 'returns the merchants items' do
-        expect(data.size).to eq(10)
-        expect(data.first[:id].to_i).to eq(items.first.id)
+        it 'returns the first 20 items' do
+          expect(data.size).to eq(20)
+          expect(data.first[:id].to_i).to eq(items.first.id)
+        end
+      end
+
+      context 'with query params' do
+        it 'can get more than 20 per page' do
+          get api_v1_items_path, params: { per_page: 25 }
+
+          expect(data.size).to eq(25)
+        end
+
+        it 'can get less than 20 per page' do
+          get api_v1_items_path, params: { per_page: 5 }
+
+          expect(data.size).to eq(5)
+        end
+
+        it 'can get different pages' do
+          get api_v1_items_path, params: { page: 2 }
+
+          result = data.map { |item| item[:id].to_i }
+
+          expect(result).not_to include(items.first.id)
+          expect(data.last[:id].to_i).to eq(items.last.id)
+        end
+
+        it 'uses page 1 for negative page numbers' do
+          get api_v1_items_path, params: { page: -2 }
+
+          result = data.map { |item| item[:id].to_i }
+          expected = Item.limit(20).map(&:id)
+
+          expect(result).to eq(expected)
+        end
       end
     end
 
-    context 'with invalid merchant id' do
-      before { get api_v1_merchant_items_path(100_000) }
+    describe 'for a merchants items' do
+      let!(:merchant2) { create :merchant }
+      let!(:other_items) { create_list :item, 4, { merchant_id: merchant2.id } }
+
+      context 'with valid merchant id' do
+        before { get api_v1_merchant_items_path(merchant.id) }
+
+        it 'returns all of the merchants items' do
+          expect(data.size).to eq(30)
+          expect(data.first[:id].to_i).to eq(items.first.id)
+        end
+      end
+
+      context 'with invalid merchant id' do
+        before { get api_v1_merchant_items_path(100_000) }
+
+        it 'returns status code 404' do
+          expect(response).to have_http_status(404)
+        end
+      end
+    end
+  end
+
+  describe 'GET /v1/items/:id' do
+    context 'with valid item' do
+      let!(:items) { create_list :item, 30, { merchant_id: merchant.id } }
+      let(:item) { items.first }
+
+      before { get api_v1_item_path(item) }
+
+      it 'returns the item' do
+        expect(data).to have_key(:id)
+        expect(data[:id].to_i).to eq(item.id)
+        expect(attributes[:name]).to eq(item.name)
+        expect(attributes[:unit_price].to_i).to eq(item.unit_price)
+      end
+    end
+
+    context 'with invalid item' do
+      before { get api_v1_item_path(100_000) }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -46,7 +116,7 @@ describe Api::V1::ItemsController do
       it 'creates an item' do
         expect(Item.count).to eq(1)
         expect(Item.last.name).to eq('value1')
-        expect(Item.last.unit_price).to eq(10_099)
+        expect(Item.last.unit_price).to eq(100.99)
       end
 
       it 'returns the item data' do
@@ -62,7 +132,7 @@ describe Api::V1::ItemsController do
       before { post api_v1_items_path, params: { item: { hell: 'world' } } }
 
       it 'returns status code 400' do
-        expect(response).to have_http_status(400)
+        expect(response).to have_http_status(404)
         expect(Item.count).to eq(0)
       end
     end
@@ -93,14 +163,64 @@ describe Api::V1::ItemsController do
     end
 
     context 'with invalid params' do
-      before { patch api_v1_item_path(item), params: {  chicken: 'hello' } }
+      before { patch api_v1_item_path(item), params: { name: 3 } }
 
       it 'does not update the item' do
-        expect(item).to eq(item.reload)
+        item.reload
+        expect(item.name).not_to eq(3)
       end
 
       it 'returns status code 400' do
         expect(response).to have_http_status(400)
+      end
+    end
+  end
+
+  describe 'DELETE /v1/items/:id' do
+    let(:merchant) { create :merchant }
+    let!(:item) { create :item, { merchant_id: merchant.id } }
+    let!(:customer) { create :customer }
+    let!(:invoice) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+    let!(:invoice_item) { create :invoice_item, { item_id: item.id, invoice_id: invoice.id } }
+
+    context 'with valid item id' do
+      it 'deletes the item' do
+        expect { delete api_v1_item_path(item) }.to change(Item, :count).by(-1)
+      end
+
+      it 'returns status code 204' do
+        delete api_v1_item_path(item)
+
+        expect(response).to have_http_status(204)
+      end
+    end
+
+    context 'with invalid item id' do
+      before { delete api_v1_item_path(-12) }
+
+      it 'returns 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    describe 'when it is the only item on the invoice' do
+      it 'deletes the invoice' do
+        delete api_v1_item_path(item)
+
+        expect(Invoice.count).to eq(0)
+        expect(merchant.invoices).to be_empty
+      end
+    end
+
+    describe 'when it is not the only item on the invoice' do
+      let!(:other_item) { create :item, { merchant_id: merchant.id } }
+      let!(:other_invoice) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+      let!(:invoice_item2) { create :invoice_item, { item_id: item.id, invoice_id: other_invoice.id } }
+      let!(:invoice_item3) { create :invoice_item, { item_id: other_item.id, invoice_id: other_invoice.id } }
+
+      it 'does not delete the item' do
+        expect { delete api_v1_item_path(other_item) }.to change(Invoice, :count).by(0)
+        expect(Invoice.count).to eq(2)
       end
     end
   end
