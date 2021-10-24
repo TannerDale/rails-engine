281b53c2ec885bba526c2473f2497590e6a115a5
Tanner Dale <tanner@tannerdale.com>
2021-10-23 22:19:06 -0500

    Add invoice delete if items invoices its called on is the only item

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,4 +8,23 @@ describe Invoice do
     it { should have_many(:items).through :invoice_items }
     it { should have_many :transactions }
   end
+
+  describe 'scopes' do
+    let(:merchant) { create :merchant }
+    let!(:item1) { create :item, { merchant_id: merchant.id } }
+    let!(:item2) { create :item, { merchant_id: merchant.id } }
+    let!(:customer) { create :customer }
+    let!(:invoice1) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+    let!(:invoice2) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+    let!(:invoice_item1) { create :invoice_item, { item_id: item1.id, invoice_id: invoice1.id } }
+    let!(:invoice_item2) { create :invoice_item, { item_id: item1.id, invoice_id: invoice2.id } }
+    let!(:invoice_item3) { create :invoice_item, { item_id: item2.id, invoice_id: invoice2.id } }
+
+
+    it 'deletes the invoices with only the item on it' do
+      item1.invoices.delete_empty
+
+      expect(Invoice.count).to eq(1)
+    end
+  end
 end
