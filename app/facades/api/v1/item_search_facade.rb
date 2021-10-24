fba8039407898cda0eff3642dd708661bb1d9230
Tanner Dale <tanner@tannerdale.com>
2021-10-24 18:41:21 -0500

    Add item searching facade with search method sorting

---
+++ app/facades/api/v1/item_search_facade.rb
@@ -0,0 +1,41 @@
+class Api::V1::ItemSearchFacade
+  class << self
+    def search(params, amount)
+      raise ActionController::BadRequest if name_and_price?(params)
+
+      results = {
+        all: -> { find_items(params) },
+        one: -> { find_items(params).first }
+      }
+      results[amount].call
+    end
+
+    private
+
+    def name_and_price?(params)
+      params[:name] && (params[:min_price] || params[:max_price])
+    end
+
+    def find_items(params)
+      if params[:name]
+        name_search(params[:name])
+      else
+        price_search(params)
+      end
+    end
+
+    def name_search(name)
+      Item.find_by_name(name)
+    end
+
+    def price_search(params)
+      if params[:min_price] && params[:max_price]
+        Item.find_in_range(params[:min_price], params[:max_price])
+      elsif params[:min_price]
+        Item.above_price(params[:min_price])
+      else
+        Item.below_price(params[:max_price])
+      end
+    end
+  end
+end
