b06cb881c0f8cb73ea23cfe1071ee45b272d2d7d
Tanner Dale <tanner@tannerdale.com>
2021-10-23 22:17:15 -0500

    Add delete empty invoices before destroy action

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  before_destroy :delete_empty_invoices
+
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
+
+  def delete_empty_invoices
+    invoices.delete_empty
+  end
 end
