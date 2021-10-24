9742ada04e8d7928496e8098da06e85cdabd20d5
Tanner Dale <tanner@tannerdale.com>
2021-10-23 22:19:34 -0500

    Add after destroy action of deleting now empty invoices

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,18 @@ describe Item do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through :invoice_items }
   end
+
+  describe 'after destroy' do
+    let(:merchant) { create :merchant }
+    let!(:item) { create :item, { merchant_id: merchant.id } }
+    let!(:customer) { create :customer }
+    let!(:invoice) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+    let!(:invoice_item) { create :invoice_item, { item_id: item.id, invoice_id: invoice.id } }
+
+    before { item.destroy }
+
+    it 'deletes invoices where it was the only item' do
+      expect(Invoice.count).to eq(0)
+    end
+  end
 end
