18c462be424ead77d081d35263ac624bf6037917
Tanner Dale <tanner@tannerdale.com>
2021-10-24 22:00:03 -0500

    Add transactions to account for added scope requirements

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,17 +14,31 @@ describe Invoice do
     let!(:item1) { create :item, { merchant_id: merchant.id } }
     let!(:item2) { create :item, { merchant_id: merchant.id } }
     let!(:customer) { create :customer }
-    let!(:invoice1) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
-    let!(:invoice2) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
-    let!(:invoice_item1) { create :invoice_item, { item_id: item1.id, invoice_id: invoice1.id } }
-    let!(:invoice_item2) { create :invoice_item, { item_id: item1.id, invoice_id: invoice2.id } }
-    let!(:invoice_item3) { create :invoice_item, { item_id: item2.id, invoice_id: invoice2.id } }
+    let!(:invoice1) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id, status: 'returned' } }
+    let!(:invoice2) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id, status: 'shipped' } }
+    let!(:invoice3) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id, status: 'packaged' } }
+    let!(:invoice4) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id, status: 'shipped' } }
+    let!(:invoice_item1) { create :invoice_item, { item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 300 } }
+    let!(:invoice_item2) { create :invoice_item, { item_id: item1.id, invoice_id: invoice2.id, quantity: 2, unit_price: 200 } }
+    let!(:invoice_item3) { create :invoice_item, { item_id: item2.id, invoice_id: invoice2.id, quantity: 3, unit_price: 50 } }
 
+    let!(:trans1) { create :transaction, { result: 'success', invoice_id: invoice1.id } }
+    let!(:trans2) { create :transaction, { result: 'success', invoice_id: invoice2.id } }
+    let!(:trans3) { create :transaction, { result: 'success', invoice_id: invoice3.id } }
+    let!(:trans4) { create :transaction, { result: 'success', invoice_id: invoice4.id } }
 
     it 'deletes the invoices with only the item on it' do
       item1.invoices.delete_empty
 
-      expect(Invoice.count).to eq(1)
+      expect(Invoice.count).to eq(3)
+    end
+
+    it 'has the shipped invoices' do
+      expect(Invoice.shipped).to eq([invoice2, invoice4])
+    end
+
+    it 'has total revenue' do
+      expect(merchant.invoices.total_revenue.map(&:revenue)).to eq([550])
     end
   end
 end
