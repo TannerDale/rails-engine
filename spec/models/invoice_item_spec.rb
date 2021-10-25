6eb875fae015c8d9f92c08798807cb72d1aaa07b
Tanner Dale <tanner@tannerdale.com>
2021-10-25 16:58:37 -0500

    Add has many relationship with transactions test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,5 +5,6 @@ describe InvoiceItem do
     it { should belong_to :invoice }
     it { should belong_to :item }
     it { should have_one(:merchant).through :item }
+    it { should have_many(:transactions).through :invoice }
   end
 end
