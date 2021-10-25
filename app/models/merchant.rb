c4041d9ce0bf4bd4efa04634fd0878efbfd29e4e
Tanner Dale <tanner@tannerdale.com>
2021-10-24 19:24:31 -0500

    Add find by name method/tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,9 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.find_by_name(name)
+    where('name ILIKE ?', "%#{name}%")
+      .order(:name)
+  end
 end
