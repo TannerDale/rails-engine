e2a846d357e6a23b40e1b206fdbb1cb1cfd51271
Tanner Dale <tanner@tannerdale.com>
2021-10-24 22:00:30 -0500

    Add scope tests for successful transactions

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,4 +5,19 @@ describe Transaction do
     it { should belong_to :invoice }
     it { should have_one(:customer).through :invoice }
   end
+
+  describe 'scopes' do
+    let(:merchant) { create :merchant }
+    let(:item) { create :item, { merchant_id: merchant.id } }
+    let(:customer) { create :customer }
+    let(:invoice) { create :invoice, { merchant_id: merchant.id, customer_id: customer.id } }
+    let!(:inv_item) { create :invoice_item, { item_id: item.id, invoice_id: invoice.id } }
+    let!(:trans1) { create :transaction, { invoice_id: invoice.id, result: 'success' } }
+    let!(:trans2) { create :transaction, { invoice_id: invoice.id, result: 'failed' } }
+    let!(:trans3) { create :transaction, { invoice_id: invoice.id, result: 'success' } }
+
+    it 'has successful transactions' do
+      expect(Transaction.success).to eq([trans1, trans3])
+    end
+  end
 end
