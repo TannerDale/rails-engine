0e7fe7afd75e29a0cd953b19a284e301b2f1d8d0
Tanner Dale <tanner@tannerdale.com>
2021-10-24 21:58:58 -0500

    Add dependent destroy on transactions, add total revenue, shipped scopes

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,12 +3,25 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 
   scope :delete_empty, -> {
     joins(:invoice_items)
       .having('COUNT(invoice_items.id) = 1')
       .group(:id)
       .destroy_all
+  }
+
+  scope :shipped, -> {
+    where(status: 'shipped')
+  }
+
+  scope :total_revenue, -> {
+    shipped
+      .joins(:transactions)
+      .merge(Transaction.success)
+      .distinct
+      .select('SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue')
+      .group(:id)
   }
 end
