c1e95420c87fd0f4cdaf3b9a5e8fc5475a5d93e7
Tanner Dale <tanner@tannerdale.com>
2021-10-24 19:24:20 -0500

    Add param validation as before action

--- app/controllers/api/v1/item_search_controller.rb
+++ app/controllers/api/v1/item_search_controller.rb
@@ -1,19 +1,25 @@
 class Api::V1::ItemSearchController < ApplicationController
-  def find
-    raise ActionController::BadRequest if empty_search_params?
+  before_action :validate_params
 
+  def find
     item = Api::V1::ItemSearchFacade.search(params, :one)
     render json: Api::V1::ItemSerializer.new(item).serializable_hash
   end
 
   def find_all
-    raise ActionController::BadRequest if empty_search_params?
-
-    item = Api::V1::ItemSearchFacade.search(params, :all)
-    render json: Api::V1::ItemSerializer.new(item).serializable_hash
+    items = Api::V1::ItemSearchFacade.search(params, :all)
+    render json: Api::V1::ItemSerializer.new(items).serializable_hash
   end
 
   private
+
+  def validate_params
+    raise ActionController::BadRequest if empty_search_params? || no_param_given?
+  end
+
+  def no_param_given?
+    !(params[:name] || params[:min_price] || params[:max_price])
+  end
 
   def empty_search_params?
     params[:name]&.empty? || params[:max_price]&.empty? || params[:min_price]&.empty?
