9c04672c7d3158c0a0a7bb5a132fd34d3ef918f1
Tanner Dale <tanner@tannerdale.com>
2021-10-25 19:02:02 -0500

    Rename method to have sold_revenue

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
   def index
     raise ActionController::BadRequest unless valid_quantity?
 
-    merchants = Merchant.ordered_by_revenue.limit(params[:quantity])
+    merchants = Merchant.ordered_by_sold_revenue.limit(params[:quantity])
     render json: Api::V1::MerchantNameRevenueSerializer.new(merchants).serializable_hash
   end
 
