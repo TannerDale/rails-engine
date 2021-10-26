1b45e424dfcf588a53869705cad4c9fb000c876b
Tanner Dale <tanner@tannerdale.com>
2021-10-25 19:03:44 -0500

    Add ordered by packaged revenue test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -30,6 +30,7 @@ describe Merchant do
     let!(:invoice_item1) { create :invoice_item, { item_id: item1.id, invoice_id: invoice1.id, quantity: 1, unit_price: 300 } }
     let!(:invoice_item2) { create :invoice_item, { item_id: item1.id, invoice_id: invoice2.id, quantity: 2, unit_price: 200 } }
     let!(:invoice_item3) { create :invoice_item, { item_id: item1.id, invoice_id: invoice2.id, quantity: 3, unit_price: 50 } }
+    let!(:invoice_item4) { create :invoice_item, { item_id: item1.id, invoice_id: invoice3.id, quantity: 3, unit_price: 50 } }
 
     let(:merchant2) { create :merchant }
     let!(:item21) { create :item, { merchant_id: merchant2.id } }
@@ -40,6 +41,7 @@ describe Merchant do
     let!(:invoice_item21) { create :invoice_item, { item_id: item21.id, invoice_id: invoice21.id, quantity: 1, unit_price: 300 } }
     let!(:invoice_item22) { create :invoice_item, { item_id: item21.id, invoice_id: invoice22.id, quantity: 8, unit_price: 25 } }
     let!(:invoice_item23) { create :invoice_item, { item_id: item21.id, invoice_id: invoice22.id, quantity: 3, unit_price: 100 } }
+    let!(:invoice_item24) { create :invoice_item, { item_id: item21.id, invoice_id: invoice23.id, quantity: 3, unit_price: 200 } }
 
     let(:merchant3) { create :merchant }
     let!(:item31) { create :item, { merchant_id: merchant3.id } }
@@ -50,6 +52,7 @@ describe Merchant do
     let!(:invoice_item31) { create :invoice_item, { item_id: item31.id, invoice_id: invoice31.id, quantity: 1, unit_price: 300 } }
     let!(:invoice_item32) { create :invoice_item, { item_id: item31.id, invoice_id: invoice32.id, quantity: 4, unit_price: 100 } }
     let!(:invoice_item33) { create :invoice_item, { item_id: item31.id, invoice_id: invoice32.id, quantity: 3, unit_price: 150 } }
+    let!(:invoice_item34) { create :invoice_item, { item_id: item31.id, invoice_id: invoice33.id, quantity: 3, unit_price: 150 } }
 
     let!(:trans1) { create :transaction, { result: 'success', invoice_id: invoice2.id } }
     let!(:trans2) { create :transaction, { result: 'success', invoice_id: invoice4.id } }
@@ -58,9 +61,16 @@ describe Merchant do
     let!(:trans5) { create :transaction, { result: 'success', invoice_id: invoice32.id } }
     let!(:trans6) { create :transaction, { result: 'success', invoice_id: invoice34.id } }
     let!(:trans7) { create :transaction, { result: 'failed', invoice_id: invoice1.id } }
+    let!(:trans8) { create :transaction, { result: 'success', invoice_id: invoice3.id } }
+    let!(:trans9) { create :transaction, { result: 'success', invoice_id: invoice23.id } }
+    let!(:trans0) { create :transaction, { result: 'success', invoice_id: invoice33.id } }
 
-    it 'has ordered by revenue' do
-      expect(Merchant.ordered_by_revenue).to eq([merchant3, merchant, merchant2])
+    it 'has ordered by sold revenue' do
+      expect(Merchant.ordered_by_sold_revenue).to eq([merchant3, merchant, merchant2])
+    end
+
+    it 'has ordered by packaged revenue' do
+      expect(Merchant.ordered_by_packaged_revenue).to eq([merchant2, merchant3, merchant])
     end
 
     it 'has ordered by items sold' do
