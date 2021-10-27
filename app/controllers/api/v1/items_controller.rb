d22bcb2588f9f8d1e0d1dddf1cb0b4c6fb415fb1
Tanner Dale <tanner@tannerdale.com>
2021-10-27 11:24:27 -0500

    Change index_item call from facade to private method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,25 +2,24 @@ class Api::V1::ItemsController < ApplicationController
   before_action :validate_merchant, only: :update
 
   def index
-    items = Api::V1::ItemFacade.index_items(params)
-    render json: Api::V1::ItemSerializer.new(items)
+    render json: V1::ItemSerializer.new(index_items)
   end
 
   def show
     item = Item.find(params[:id])
-    render json: Api::V1::ItemSerializer.new(item)
+    render json: V1::ItemSerializer.new(item)
   end
 
   def create
     item = Item.create!(item_params)
-    render json: Api::V1::ItemSerializer.new(item), status: 201
+    render json: V1::ItemSerializer.new(item), status: 201
   end
 
   def update
     item = Item.find(params[:id])
 
     item.update(item_params)
-    render json: Api::V1::ItemSerializer.new(item)
+    render json: V1::ItemSerializer.new(item)
   end
 
   def destroy
