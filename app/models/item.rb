2d9fb948f3f77f2ed66646b49d81fed5f1611fa3
Tanner Dale <tanner@tannerdale.com>
2021-10-27 11:28:38 -0500

    Add bangs to destructive delete_empty_invoices method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  before_destroy :delete_empty_invoices
+  before_destroy :delete_empty_invoices!
 
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
 
-  def delete_empty_invoices
-    invoices.delete_empty
+  def delete_empty_invoices!
+    invoices.delete_empty!
   end
 
   def self.find_by_name(name)
