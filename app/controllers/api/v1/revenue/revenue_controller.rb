d3007ce93a7c17d02c635c12401b74ea6666e134
Tanner Dale <tanner@tannerdale.com>
2021-10-27 22:42:44 -0500

    Add revenue from across date ranges

--- app/controllers/api/v1/revenue/revenue_controller.rb
+++ app/controllers/api/v1/revenue/revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Revenue::RevenueController < ApplicationController
-  before_action :validate_params, only: :unshipped
+  before_action :validate_quantity, only: :unshipped
 
   def unshipped
     results = Merchant.ordered_by_packaged_revenue.limit(params[:quantity])
@@ -11,9 +11,34 @@ class Api::V1::Revenue::RevenueController < ApplicationController
     render json: V1::WeeklyRevenueSerializer.new(weeks)
   end
 
+  def range
+    validate_date_params
+
+    start, stop = validate_dates
+    revenue = Invoice.revenue_range(start, stop + 1)[0]
+    render json: V1::RevenueSerializer.new(revenue)
+  end
+
   private
 
-  def validate_params
+  def validate_dates
+    start = Date.parse(params[:start])
+    stop = Date.parse(params[:end])
+
+    raise ActionController::BadRequest unless start < stop
+
+    [start, stop]
+  end
+
+  def validate_date_params
+    raise ActionController::BadRequest unless present_date_params?
+  end
+
+  def present_date_params?
+    params[:start].present? && params[:end].present?
+  end
+
+  def validate_quantity
     params[:quantity] ||= 10
     raise ActionController::BadRequest unless params[:quantity]&.to_i&.positive?
   end
