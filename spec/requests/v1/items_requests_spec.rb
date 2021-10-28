8932ae8eeedeffed2f9f165788cb923f0aded456
Tanner Dale <tanner@tannerdale.com>
2021-10-28 10:04:10 -0500

    Change expected status codes

--- spec/requests/v1/items_requests_spec.rb
+++ spec/requests/v1/items_requests_spec.rb
@@ -133,7 +133,7 @@ describe Api::V1::ItemsController do
       before { post api_v1_items_path, params: { item: { hell: 'world' } } }
 
       it 'returns status code 400' do
-        expect(response).to have_http_status(404)
+        expect(response).to have_http_status(400)
         expect(Item.count).to eq(0)
       end
     end
@@ -178,7 +178,7 @@ describe Api::V1::ItemsController do
       it 'does not update with invalid merchant id' do
         patch api_v1_item_path(item), params: { item: { merchant_id: 11111 } }
 
-        expect(response).to have_http_status(400)
+        expect(response).to have_http_status(404)
       end
     end
   end
