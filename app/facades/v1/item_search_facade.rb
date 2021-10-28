219a26d91b035516fd72e8a9089cde3f747a927b
Tanner Dale <tanner@tannerdale.com>
2021-10-28 13:34:01 -0500

    Refactor searching, remove helper methods

--- app/facades/v1/item_search_facade.rb
+++ app/facades/v1/item_search_facade.rb
@@ -12,23 +12,9 @@ class V1::ItemSearchFacade
 
     def find_items(params)
       if params[:name]
-        name_search(params[:name])
+        Item.find_by_name(params[:name])
       else
-        price_search(params)
-      end
-    end
-
-    def name_search(name)
-      Item.find_by_name(name)
-    end
-
-    def price_search(params)
-      if params[:min_price] && params[:max_price]
         Item.find_in_range(params[:min_price], params[:max_price])
-      elsif params[:min_price]
-        Item.above_price(params[:min_price])
-      else
-        Item.below_price(params[:max_price])
       end
     end
   end
