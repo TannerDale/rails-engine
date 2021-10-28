75265a58e8b78c154418273ca69a53f522efbb97
Tanner Dale <tanner@tannerdale.com>
2021-10-27 22:41:43 -0500

    Remove unneeded seriable_hash method call

--- app/controllers/api/v1/revenue/merchants_controller.rb
+++ app/controllers/api/v1/revenue/merchants_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
     raise ActionController::BadRequest unless valid_quantity?
 
     merchants = Merchant.ordered_by_sold_revenue.limit(params[:quantity])
-    render json: V1::MerchantNameRevenueSerializer.new(merchants).serializable_hash
+    render json: V1::MerchantNameRevenueSerializer.new(merchants)
   end
 
   def show
